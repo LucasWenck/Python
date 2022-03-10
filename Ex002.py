7ed7ae29a23c999a16a6af46166556ca0dc42188
LucasWenck <99269324+LucasWenck@users.noreply.github.com>
2022-03-10 19:51:54 -0300

    Add files via upload

---
+++ Ex002.py
@@ -0,0 +1,2 @@
+nom = input('Informe o seu nome por favor\n')
+print('Boas vindas, {} e um prazer te conhecer!'.format(nom))
---
+++ Ex003.py
@@ -0,0 +1,3 @@
+a = int(input('Digite o primeiro numero: '))
+b = int(input('Digite o segundo numero: '))
+print('A soma entre {} e {} e igual a {}.'.format(a, b, a+b))
